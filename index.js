da3748788bf00991a980548c4df3753cebda5003
Michael Cultrera <michaelcultrera@Michaels-MacBook-Pro.local>
2022-01-14 00:08:09 -0500

    *

--- index.js
+++ index.js
@@ -1,26 +1,86 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-// function myFunction() {
-//     document.getElementById("myDropdown").classList.toggle("show");
-//   }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  // window.onclick = function(event) {
-  //   if (!event.target.matches('#dropbtn')) {
-  //     var dropdowns = document.getElementById("dropdown-content");
-  //     var i;
-  //     for (i = 0; i < dropdowns.length; i++) {
-  //       var openDropdown = dropdowns[i];
-  //       if (openDropdown.classList.contains('show')) {
-  //         openDropdown.classList.remove('show');
-  //       }
-  //     }
-  //   }
-  // }
 
-// JQUERY
-  
+$("#dropdown").click(function (e) {
+  e.stopPropagation();
+});
 
+$(function () {
+  $("#dropdown").click(function () {
+    $("#dropdown-content").slideDown("slow", function () {
+      //Animation complete
+    })
+  });
+  $(document).click(function () {
+    $("#dropdown-content").hide();
+  });
 
+  $("#nameinput").mouseenter(function () {
+    $(this).css({
+      border: '0 solid #8fa0ef'
+    }).stop(true, false).animate({
+      borderWidth: 3
+    }, 200);
+  }).mouseleave(function () {
+    $(this).stop(true, false).animate({
+      borderWidth: 0
+    }, 200);
+  });
+  $("#emailinput").mouseenter(function () {
+    $(this).css({
+      border: '0 solid #8fa0ef'
+    }).stop(true, false).animate({
+      borderWidth: 3
+    }, 200);
+  }).mouseleave(function () {
+    $(this).stop(true, false).animate({
+      borderWidth: 0
+    }, 200);
+  });
+  $("#messageinput").mouseenter(function () {
+    $(this).css({
+      border: '0 solid #8fa0ef'
+    }).stop(true, false).animate({
+      borderWidth: 3
+    }, 200);
+  }).mouseleave(function () {
+    $(this).stop(true, false).animate({
+      borderWidth: 0
+    }, 200);
+  });
 
-
+  $("#resumelink").hover(function () {
+    $(this).stop(true, false).animate({
+      fontSize: "17px"
+    }, 400);
+  }, function () {
+    $(this).stop(true, false).animate({
+      fontSize: "15px"
+    });
+  });
+  $("#projectslink").hover(function () {
+    $(this).stop(true, false).animate({
+      fontSize: "17px"
+    }, 400);
+  }, function () {
+    $(this).stop(true, false).animate({
+      fontSize: "15px"
+    });
+  });
+  $("#contactlink").hover(function () {
+    $(this).stop(true, false).animate({
+      fontSize: "17px"
+    }, 400);
+  }, function () {
+    $(this).stop(true, false).animate({
+      fontSize: "15px"
+    });
+  });
+  $("#submitbtn").hover(function () {
+    $(this).stop(true, false).animate({
+      fontSize: "16.5px"
+    }, 100);
+  }, function () {
+    $(this).stop(true, false).animate({
+      fontSize: "16px"
+    });
+  });
+});
